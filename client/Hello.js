00c6f85353a9a9b352796b735906ebfbabfdba23
Sarath <sarath.hacks@gmail.com>
2016-11-05 20:24:37 +0530

    Initail commit

---
+++ client/Hello.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var Hello = React.createClass({
+  render:function(){
+    return React.createElement('h1',null,'Hello World!');
+  }
+});
+
+module.exports = Hello;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+
+var Hello = require('./client/Hello');
+
+var app = express();
+
+app.get('/',function(req,res){
+
+  var html = ReactDOMServer.renderToString(
+    React.createElement(Hello)
+  )
+
+  res.send(html);
+
+});
+
+
+
+app.listen(3000);
+console.log("App listening on port 3000");
